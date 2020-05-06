183cd3b61851cc8f5a9b41e76e700c1f9c1178e0
anvitadixit <anvitadixit@outlook.com>
2020-05-06 11:12:42 +0530

    handout

--- src/screens/handouts/Handouts.js
+++ src/screens/handouts/Handouts.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 import MasterLayout from '../../components/Layout/MasterLayout';
-
+import AsyncStorage from '@react-native-community/async-storage';
 export default class Handouts extends Component {
   render() {
     return (
       <MasterLayout>
         <View>
           <Text> Handouts </Text>
+          <Button
+            title="Actually, sign me out :)"
+            onPress={this._signOutAsync}
+          />
         </View>
       </MasterLayout>
     );
   }
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+  };
 }
