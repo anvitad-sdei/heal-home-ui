549118238406f8af1ac79b10865562fa638d81c5
anvitad-sdei <anvitadixit.ss@smartdatainc.net>
2020-05-13 15:41:24 +0530

    CustomDateTimepicker component

---
+++ src/components/DateTimePicker/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {View} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import normalize from '../../helpers/ResponsiveFont';
+export const CustomDatePicker = ({date, handler}) => {
+  return (
+    <View
+      style={{
+        paddingBottom: normalize(10),
+      }}>
+      <DateTimePicker
+        testID="dateTimePicker"
+        timeZoneOffsetInMinutes={0}
+        value={new Date(date)}
+        mode={'date'}
+        is24Hour={true}
+        display="default"
+        onChange={handler}
+      />
+    </View>
+  );
+};
+
+export const CustomTimePicker = ({time, handler}) => {
+  return (
+    <View
+      style={{
+        paddingBottom: normalize(10),
+      }}>
+      <DateTimePicker
+        testID="dateTimePicker"
+        value={new Date(time)}
+        mode="time"
+        is24Hour={true}
+        display="default"
+        onChange={handler}
+      />
+    </View>
+  );
+};
