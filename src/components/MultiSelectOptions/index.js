da265addaf64cd80c41f914d6733b918f153ad6d
anvitad-sdei <anvitadixit.ss@smartdatainc.net>
2020-05-22 10:00:03 +0530

    assesment logic added 2

--- src/components/MultiSelectOptions/index.js
+++ src/components/MultiSelectOptions/index.js
@@ -3,37 +3,49 @@ import normalize from '../../helpers/ResponsiveFont';
 import {View, StyleSheet} from 'react-native';
 import CustomRadioButton from '../CustomRadioButton.js';
 
-const MultiSelectOptions = ({status, handler}) => {
+const MultiSelectOptions = ({
+  handler,
+  mq1,
+  mq2,
+  mq3,
+  mq4,
+  mq5,
+  mq1Handler,
+  mq2Handler,
+  mq3Handler,
+  mq4Handler,
+  mq5Handler,
+}) => {
   return (
     <View style={styles.container}>
       <CustomRadioButton
         title="Never"
-        status={status}
-        handler={handler}
+        status={mq1 || false}
+        handler={mq1Handler}
         radioButtonStyle={styles.radioButtonStyle}
       />
       <CustomRadioButton
         title="Monthly or Less"
-        status={!status}
-        handler={handler}
+        status={mq2 || false}
+        handler={mq2Handler}
         radioButtonStyle={styles.radioButtonStyle}
       />
       <CustomRadioButton
         title="Monthly"
-        status={!status}
-        handler={handler}
+        status={mq3 || false}
+        handler={mq3Handler}
         radioButtonStyle={styles.radioButtonStyle}
       />
       <CustomRadioButton
         title="Weekly"
-        status={!status}
-        handler={handler}
+        status={mq4 || false}
+        handler={mq4Handler}
         radioButtonStyle={styles.radioButtonStyle}
       />
       <CustomRadioButton
         title="Daily or almost daily"
-        status={!status}
-        handler={handler}
+        status={mq5 || false}
+        handler={mq5Handler}
         radioButtonStyle={styles.radioButtonStyle}
       />
     </View>
